830634458e75b9a3b6e1489fdf97b4c133187a4f
jini <jini@jiniworld.me>
2022-02-14 01:34:01 +0900

    prod profile 설정 추가

--- src/main/java/xyz/applebox/book/config/OpenApiConfig.java
+++ src/main/java/xyz/applebox/book/config/OpenApiConfig.java
@@ -4,18 +4,25 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class OpenApiConfig {
     @Bean
-    public OpenAPI openAPI(@Value("${book.version}") String appVersion, @Value("${spring.profiles.active}") String active) {
+    public OpenAPI openAPI(@Value("${book.version}") String appVersion,
+                           @Value("${book.url}") String url, @Value("${spring.profiles.active}") String active) {
         Info info = new Info().title("Book API - " + active).version(appVersion)
                 .description("Book API입니다.")
                 .contact(new Contact().name("jini").url("https://blog.jiniworld.me/").email("jini@jiniworld.me"))
                 .license(new License().name("MIT License").url("https://github.com/jiniya22/book/blob/master/LICENSE"));
+
+        List<Server> servers = Arrays.asList(new Server().url(url).description("book (" + active +")"));
 
 //        SecurityScheme securityScheme = new SecurityScheme()
 //                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
@@ -26,6 +33,7 @@ public class OpenApiConfig {
 //                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
 //                .addSecurityItem(schemaRequirement)
 //                .security(Arrays.asList(schemaRequirement))
-                .info(info);
+                .info(info)
+                .servers(servers);
     }
 }
