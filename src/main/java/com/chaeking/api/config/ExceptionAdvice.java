7c8bd9175e6deb48652755972f2929cd3b9d02ca
jini <jini@jiniworld.me>
2022-05-26 00:55:39 +0900

    1. spring-boot-starter-validation dependency 추가 2. validation Exception Handler 설정

--- src/main/java/com/chaeking/api/config/ExceptionAdvice.java
+++ src/main/java/com/chaeking/api/config/ExceptionAdvice.java
@@ -3,17 +3,30 @@ package com.chaeking.api.config;
 import com.chaeking.api.config.exception.InvalidInputException;
 import com.chaeking.api.domain.value.response.BaseResponse;
 import com.chaeking.api.util.MessageUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ValidationException;
 
 @ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(InvalidInputException.class)
-    protected ResponseEntity<BaseResponse> invalidInputException(InvalidInputException e) {
+    @ExceptionHandler({InvalidInputException.class, ValidationException.class})
+    protected ResponseEntity<BaseResponse> invalidInputException(RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(BaseResponse.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<BaseResponse> handleException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .findFirst().map(fieldError ->
+                        String.format("%s 오류. %s", fieldError.getField(), fieldError.getDefaultMessage()))
+                .orElse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(BaseResponse.of(message));
     }
 
 }
--- src/main/java/com/chaeking/api/domain/value/Ttt.java
+++ src/main/java/com/chaeking/api/domain/value/Ttt.java
@@ -13,5 +13,5 @@ public record Ttt(
         @Schema(description = "이름") @NotBlank String name,
 //        @Schema(description = "생년월일(yyyy-MM-dd)") @NotBlank @Pattern(regexp = "^(19[0-9][0-9]|20\\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$") String birthDate,
 //                @Schema(description = "생년월일(yyyy-MM-dd)") LocalDate birthDate,
-        @Schema(description = "성별") @Pattern(regexp = "^[MF]$") Sex sex
+        @Schema(description = "성별") @Pattern(regexp = "^[MF]$") String sex
 ) {}
