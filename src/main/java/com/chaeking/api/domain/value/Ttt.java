5c155210ec77e98ddf6d40e0c219385c2b7edf6b
jini <jini@jiniworld.me>
2022-05-28 01:17:52 +0900

    VlidationMessages.properties 파일 추가

--- src/main/java/com/chaeking/api/domain/value/Ttt.java
+++ src/main/java/com/chaeking/api/domain/value/Ttt.java
@@ -1,6 +1,5 @@
 package com.chaeking.api.domain.value;
 
-import com.chaeking.api.domain.enumerate.Sex;
 import com.chaeking.api.util.RegexpUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -14,6 +13,6 @@ public record Ttt(
         @Schema(description = "이름") @Pattern(regexp = "^[a-zA-Z가-힣]{1,10}$") String name,
         @Schema(description = "생년월일(yyyy-MM-dd)") @NotBlank @Pattern(regexp = RegexpUtils.DATE) String birthDate,
         @Pattern(regexp = RegexpUtils.DATETIME) String dateTime,
-        @Schema(description = "성별") Sex sex
+        @Schema(description = "성별") @Pattern(regexp = RegexpUtils.SEX) String sex
 ) {
 }
