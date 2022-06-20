91ffe520bfd043492705bdbea7cc2fe75f2999e4
jiniya22 <jini@jiniworld.me>
2022-06-20 16:00:50 +0900

    :fire: 불필요한 코드 제거

--- src/main/java/com/chaeking/api/controller/temp/BookSearchController.java
+++ src/main/java/com/chaeking/api/controller/temp/BookSearchController.java
@@ -1,6 +1,5 @@
 package com.chaeking.api.controller.temp;
 
-import com.chaeking.api.domain.value.naver.KakaoBookValue;
 import com.chaeking.api.domain.value.naver.NaverBookValue;
 import com.chaeking.api.domain.value.response.DataResponse;
 import com.chaeking.api.service.BookService;
@@ -17,7 +16,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
-@Tag(name = "temp", description = "(테스트용) 암호화, 네이버/카카오 책 검색")
+@Tag(name = "temp", description = "(테스트용) 암호화, 네이버 책검색")
 @RequiredArgsConstructor
 @RequestMapping("/temp")
 @RestController
@@ -48,36 +47,4 @@ public class BookSearchController {
         return DataResponse.of(res);
     }
 
-    @Operation(summary = "카카오 책 검색 및 저장",
-            description = """
-                    <ul>
-                        <li>page: 1~100 사이의 값 (default: 1)</li>
-                        <li>size: 1~50 사이의 값 (default: 10)</li>
-                        <li>검색 필드 제한
-                            <ul>
-                                <li>title: 제목</li>
-                                <li>isbn: isbn</li>
-                                <li>publisher: 출판사</li>
-                                <li>person: 인명</li>
-                            </ul>
-                        </li>
-                        <li>정렬 옵션
-                            <ul>
-                                <li>accuracy: 정확도순</li>
-                                <li>latest: 발간일순</li>
-                            </ul>
-                        </li>
-                    </ul>
-                    """
-    )
-    @GetMapping("/book-kakao")
-    public DataResponse<KakaoBookValue.Res.BookBasic> searchKakaoBook(
-            @Parameter(description = "검색어") @RequestParam @NotBlank String query,
-            @Parameter(description = "검색 필드 제한") @RequestParam(required = false) String target,
-            @Parameter(description = "정렬 옵션") @RequestParam(defaultValue = "accuracy") String sort,
-            @RequestParam(required = false, defaultValue = "1") @Min(1) @Max(100) int page,
-            @RequestParam(required = false, defaultValue = "10") @Min(1) @Max(50) int size) {
-        KakaoBookValue.Res.BookBasic res = bookService.searchKakaoBook(query, target, sort, page, size);
-        return DataResponse.of(res);
-    }
 }
