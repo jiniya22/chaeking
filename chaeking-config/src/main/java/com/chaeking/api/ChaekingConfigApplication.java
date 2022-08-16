53c079732c0d376d38f18f718d13eba6404e1294
jini <jini@jiniworld.me>
2022-08-16 16:31:26 +0900

    :bug: spring cloud release train 관련 bug fix

--- chaeking-config/src/main/java/com/chaeking/api/ChaekingConfigApplication.java
+++ chaeking-config/src/main/java/com/chaeking/api/ChaekingConfigApplication.java
@@ -1,25 +1,40 @@
 package com.chaeking.api;
 
 import com.chaeking.api.config.DatasourceConfig;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@RequiredArgsConstructor
+import javax.annotation.PostConstruct;
+
 @EnableConfigurationProperties(DatasourceConfig.class)
 @SpringBootApplication
-public class ChaekingConfigApplication implements CommandLineRunner {
-    private final DatasourceConfig datasourceConfig;
+public class ChaekingConfigApplication {
 
     public static void main(String ar[]) {
-        SpringApplication.run(DatasourceConfig.class, ar);
+        SpringApplication.run(ChaekingConfigApplication.class, ar);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.printf("test {} , {}", datasourceConfig.getUserName(), datasourceConfig.getPassword());
+    static String username;
+
+    static String password;
+
+    @Value("${datasource.username}")
+    public void setUsername(String username) {
+        ChaekingConfigApplication.username = username;
     }
 
+    @Value("${datasource.password}")
+    public void setPassword(String password) {
+        ChaekingConfigApplication.password = password;
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("##########################");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println("##########################");
+    }
 }
