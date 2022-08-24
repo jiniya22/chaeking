cbee54e8b166ce13fad535d3971cf045d4321e5b
jini <jini@jiniworld.me>
2022-08-24 14:42:41 +0900

    :bug: -

--- chaeking-api/src/main/java/com/chaeking/api/controller/v1/setting/system/MetaController.java
+++ chaeking-api/src/main/java/com/chaeking/api/controller/v1/setting/system/MetaController.java
@@ -4,6 +4,7 @@ import com.chaeking.api.domain.enumerate.MetaType;
 import com.chaeking.api.domain.value.response.DataResponse;
 import com.chaeking.api.service.MetaService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SecurityRequirements
 @RequiredArgsConstructor
 @Tag(name = "setting-system", description = "설정-시스템(공지사항, FAQ, 이용약관, 메타정보 등)")
 @RestController
