cd7f510ab83df9ef02f28194d7aa5b35d4f6749f
jini <jini@jiniworld.me>
2023-01-17 19:05:29 +0900

    :white_check_mark: notice test 코드에 given 설정 추가

--- chaeking-api-hexagon/src/test/java/com/chaeking/api/notice/adapter/in/web/GetNoticeControllerTest.java
+++ chaeking-api-hexagon/src/test/java/com/chaeking/api/notice/adapter/in/web/GetNoticeControllerTest.java
@@ -1,25 +1,31 @@
 package com.chaeking.api.notice.adapter.in.web;
 
 import com.chaeking.api.notice.application.port.in.GetNoticeQuery;
+import com.chaeking.api.notice.application.port.out.NoticeDetail;
+import com.chaeking.api.notice.application.port.out.NoticeSimple;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 //@AutoConfigureMockMvc
@@ -43,6 +49,11 @@ class GetNoticeControllerTest {
 
     @Test
     void notices() throws Exception {
+        given(getNoticeQuery.getNoticeSimples(PageRequest.of(0, 3, Sort.by(Sort.Order.desc("id")))))
+                .willReturn(List.of(new NoticeSimple(3L, "공지사항 3", "2023-01-15 09:30:00"),
+                        new NoticeSimple(2L, "공지사항 2", "2023-01-10 18:00:00"),
+                        new NoticeSimple(1L, "공지사항 1", "2023-01-09 09:30:00")));
+        
         ResultActions resultActions = mockMvc.perform(
                         MockMvcRequestBuilders.get("/v1/notices")
                                 .header("Content-Type", "application/json")
@@ -64,11 +75,24 @@ class GetNoticeControllerTest {
 
     @Test
     void notice() throws Exception {
-        mockMvc.perform(
-                        MockMvcRequestBuilders.get("/v1/notices/{id}", 1)
+        given(getNoticeQuery.getNoticeDetail(1L))
+                .willReturn(new NoticeDetail(1L, "공지사항 1", "첫번째 공지사항입니다!!", "2023-01-09"));
+        
+        ResultActions resultActions = mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/notices/{id}", 1)
                                 .header("Content-Type", "application/json")
                 )
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+
+        resultActions
+                .andDo(print())
+                .andDo(document("notice-get",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("id").description("공지사항 id")
+                        )
+                ));
     }
 }
