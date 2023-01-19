39cc77b48941a436d1c9bc94fe9ebfcc5f6387e8
jini <jini@jiniworld.me>
2023-01-19 14:53:24 +0900

    :bug: file read error 수정 > cannot be resolved to absolute file path because it does not reside in the file system

--- chaeking-api-hexagon/src/main/java/com/chaeking/api/IndexController.java
+++ chaeking-api-hexagon/src/main/java/com/chaeking/api/IndexController.java
@@ -1,13 +1,13 @@
 package com.chaeking.api;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +17,7 @@ import java.util.List;
 public class IndexController {
 
     @GetMapping("")
-    public ModelAndView index() {
+    public ModelAndView index() throws IOException {
         ModelAndView mv = new ModelAndView("index");
         List<String> apiDocNames = getApiDocs();
         mv.addObject("apiDocs", apiDocNames);
@@ -26,16 +26,10 @@ public class IndexController {
 
     private List<String> getApiDocs() {
         try {
-            File docsDirectory = ResourceUtils.getFile("classpath:static/docs");
-            File[] docs = docsDirectory.listFiles();
-            if (docs != null) {
-                List<String> fileNames = Arrays.stream(docs)
-                        .map(File::getName)
-                        .filter(name -> name.endsWith(".html")).toList();
-                System.out.println(fileNames);
-                return fileNames;
-            }
-        } catch (FileNotFoundException e) {
+            InputStream inputStream = new ClassPathResource("static/docs").getInputStream();
+            String files = new String(inputStream.readAllBytes());
+            return Arrays.asList(files.split("\n"));
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
         return Collections.emptyList();
