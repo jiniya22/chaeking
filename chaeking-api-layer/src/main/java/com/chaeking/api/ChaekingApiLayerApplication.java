ac2f4f3c1e199135898ef5caf462b0eea1e836b3
jini <jini@jiniworld.me>
2023-02-13 11:21:26 +0900

    :fire:

--- chaeking-api-layer/src/main/java/com/chaeking/api/ChaekingApiLayerApplication.java
+++ chaeking-api-layer/src/main/java/com/chaeking/api/ChaekingApiLayerApplication.java
@@ -10,7 +10,6 @@ public class ChaekingApiLayerApplication {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.KOREA);
-        System.setProperty("spring.mail.password", System.getenv("CHAEKING_MAIL"));
         SpringApplication.run(ChaekingApiLayerApplication.class, args);
     }
 
